e1ff354cec5439e6542db92b9f85ff79df09b227
DiedCamus <35491489+DiedCamus@users.noreply.github.com>
2019-07-23 23:18:29 +0800

    NO7 complete

---
+++ src/NO7.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//Given a 32-bit signed integer, reverse digits of an integer.
+//Example 1:
+//        Input: 123
+//        Output: 321
+//Example 2:
+//        Input: -123
+//        Output: -321
+//Example 3:
+//        Input: 120
+//        Output: 21
+public class NO7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(reverse(num));
+    }
+    public static int reverse(int x) {
+        int result = 0;
+        while (x != 0){
+            int single_digit = 0;
+            single_digit = x % 10;
+            x = x / 10;
+            if (result > Integer.MAX_VALUE/10 || (result == Integer.MAX_VALUE / 10 && single_digit > 7)) return 0;
+            if (result < Integer.MIN_VALUE/10 || (result == Integer.MIN_VALUE / 10 && single_digit < -8)) return 0;
+            result = result * 10 + single_digit;
+        }
+        return result;
+    }
+
+}
