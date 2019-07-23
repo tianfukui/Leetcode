157d5a9277be85cbd3e841f754d7c8ddf46b69fa
tianfukui <tianfukui@pku.edu.cn>
2019-07-24 00:10:17 +0800

    NO0011 complete

--- src/NO0011.java
+++ src/NO0011.java
@@ -25,7 +25,7 @@ public class NO0011 {
         return area;
     }
     public static int maxArea_Brute_Force(int[] height) {
-        //时间复杂度O(n^2) 空间复杂度O(1)
+        //时间复杂度O(n^2),空间复杂度O(1)
         int area = 0;
         for(int i = 0; i < height.length; i++){
             for(int j = i + 1; j < height.length; j ++){
