4ca313a89f9ca7d445262c10fc715ab64ccfd0e2
tianfukui <tianfukui@pku.edu.cn>
2019-07-26 00:05:20 +0800

    NO0015 complete

--- src/NO0015.java
+++ src/NO0015.java
@@ -14,6 +14,7 @@ import java.util.List;
 //        ]
 public class NO0015 {
     public List<List<Integer>> threeSum(int[] nums) {
+        //时间复杂度O(n^2),空间复杂度O(1)
         List<List<Integer>> list = new ArrayList<>();
         if(nums.length < 3) return list;
         int len = nums.length;
