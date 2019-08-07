f04336a929d7e935f5aed4f70d8ba63f3584c531
tianfukui <1135606668@qq.com>
2019-08-07 17:04:13 +0800

    NO.17 complete

---
+++ src/NO0017.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NO0017 {
+    private final String[] pad = {
+            "","*","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+    };
+    public List<String> letterCombinations(String digits){
+        List<String> ret = new ArrayList<>();
+        int length = digits.length();
+        if(length == 0) return ret;
+        else if(length == 1){
+            for(char c:pad[Integer.parseInt(digits.charAt(0) + "")].toCharArray()){
+                ret.add("" + c);
+            }
+        }
+        else{
+            List<String> temp = new ArrayList<>();
+            temp.addAll(letterCombinations(digits.substring(1)));
+            for(String s:temp){
+                for(char c:pad[Integer.parseInt(digits.charAt(0) + "")].toCharArray()){
+                    ret.add(c + s);
+                }
+            }
+        }
+        return ret;
+    }
+
+}
