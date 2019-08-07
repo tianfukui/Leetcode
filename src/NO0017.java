361e8333d54c38e3f01be548b458acd52cc8d669
tianfukui <1135606668@qq.com>
2019-08-07 22:25:59 +0800

    NO0017 complete

--- src/NO0017.java
+++ src/NO0017.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+//A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+//Example:
+//        Input: "23"
+//        Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
 public class NO0017 {
     private final String[] pad = {
             "","*","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
